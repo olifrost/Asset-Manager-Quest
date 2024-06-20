ba548ac945d8c34a39d9ae9e11b23af21d143b8d
Oli Frost <mail@olifro.st>
2024-06-20 21:03:51 +0100

    Small Change

--- options/devmode.js
+++ options/devmode.js
@@ -1,12 +1,6 @@
 function DevMode() {
     // Set the variables
     protect = false;
-
-    // Hide the splash screen element
-    const splashScreen = document.getElementById('splash-screen');
-    if (splashScreen) {
-        splashScreen.style.display = 'none';
-    }
 }
 
 DevMode();
